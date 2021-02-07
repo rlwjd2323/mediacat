1757d420db5274a6657bae611c42665ddb61bab3
rlwjd2323 <stygj@naver.com>
2021-02-07 21:08:56 +0900

    구조변경

--- runCrawler.sh
+++ runCrawler.sh
@@ -1,15 +1,15 @@
 #!bin/bash
 
 # 파일 저장 경로
-export path='/data/NewsData/'
-export ext='txt'
+export path=''
+export ext=''
 
 #db 접속 정보
-export id='solugate'
-export pw='!epdlxj23'
-export host='192.168.0.238'
-export port='3306'
-export db='NewsCrawler'
+export id=''
+export pw=''
+export host=''
+export port=''
+export db=''
 
 # 크롤러 실행
-/mnt/d/workpython/anaconda3/envs/NewsCrawler/bin/python /mnt/d/workpython/workspace/NewsCrawler/runCrawler.py
+anaconda3/bin/python runCrawler.py
